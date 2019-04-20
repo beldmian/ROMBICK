b29e5a6da5f06bd4175b172e0d8d9dbddee73d24
beldmian <beldmian@gmail.com>
2019-04-20 21:18:10 +0300

    Modify Users App Add Registration and etc Modify Navbar Add Messages

--- ROMBICK/settings.py
+++ ROMBICK/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'homepage'
